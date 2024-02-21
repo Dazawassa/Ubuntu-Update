7bf44437ff7c651efa93fc1cd2152218caa24662
Darcy McBeth <jackmcbeth28469@gmail.com>
2024-02-21 12:36:18 +0000

    Added support for snaps and flatpak

--- Update Ubuntu.sh
+++ Update Ubuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo snap refresh && flatpak update
